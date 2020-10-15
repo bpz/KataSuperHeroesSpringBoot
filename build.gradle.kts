cbe86512705a82d7fc9abb61e000cd8e9eee5834
Beatriz Hernandez <beatriz@gokarumi.com>
2020-10-15 16:07:29 +0200

    Add H2 in build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  runtimeOnly("com.h2database:h2")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
   testImplementation("org.springframework.boot:spring-boot-starter-test"){
