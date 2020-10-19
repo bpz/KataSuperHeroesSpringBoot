0c1ee8eee693627eba6e2f7e6a41e97770d27140
Beatriz Hernandez <beatriz@gokarumi.com>
2020-10-19 17:33:17 +0200

    Use the datasource in the repository
    - It's a dependency inside repository
    - Add map from entity to domain
    - Update test

--- src/main/kotlin/com/karumi/superhero/data/model/SuperHeroesDataMappers.kt
+++ src/main/kotlin/com/karumi/superhero/data/model/SuperHeroesDataMappers.kt
@@ -3,9 +3,15 @@ package com.karumi.superhero.data.model
 import com.karumi.superhero.domain.model.NewSuperHero
 import com.karumi.superhero.domain.model.SuperHero
 
-fun NewSuperHero.mapToSuperHero(newId: String) =
+fun NewSuperHero.mapToSuperHeroEntity(): SuperHeroEntity =
+  SuperHeroEntity(
+    name = this.name,
+    photo = this.photo
+  )
+
+fun SuperHeroEntity.mapToDomain() =
   SuperHero(
-    id = newId,
+    id = this.id.toString(),
     name = this.name,
     photo = this.photo
   )
